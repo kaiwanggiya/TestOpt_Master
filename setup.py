a8fe4d31bd7e482fca3d8f4369ee42ab8eed5708
kaiwanggiya <kurok_wang@hotmail.com>
2017-08-03 22:13:40 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'testopt',
+    version = '0.1',
+    keywords='test optimize',
+    description = 'a library for TestOpt',
+    license = 'MIT License',
+    url = 'https://github.com/kaiwanggiya/TestOpt_Master',
+    author = 'Kai WANG',
+    author_email = 'kai.3.wang@nokia.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
